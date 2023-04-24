9214845f6f871e7885b9d88970bec8fb7a09bb87
Ludovic Dupre <dupreludovic1@gmail.com>
2023-04-24 14:37:54 +0200

    carT Logic commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import { ProductsHeaderComponent } from './pages/home/components/products-header
 import { FiltersComponent } from './components/filters/filters.component';
 import { ProductBoxComponent } from './pages/product-box/product-box.component';
 import { CartComponent } from './pages/cart/cart.component';
+import { CartService } from './services/cart.service';
 
 @NgModule({
   declarations: [
@@ -51,7 +52,7 @@ import { CartComponent } from './pages/cart/cart.component';
     MatBadgeModule,
     MatSnackBarModule
   ],
-  providers: [],
+  providers: [CartService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/pages/product-box/product-box.component.ts
+++ src/app/pages/product-box/product-box.component.ts
@@ -1,11 +1,31 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-product-box',
   templateUrl: './product-box.component.html'
 })
-export class ProductBoxComponent {
+export class ProductBoxComponent implements OnInit {
 
   @Input() fullWidthMode = false;
+
+  product : Product | undefined = {
+    id:1,
+    title : 'Sneakers',
+    price : 150,
+    category:'shoes',
+    description : 'Description',
+    image : 'https://via.placeholder.com/150'
+  };
+
+  @Output() addToCart = new EventEmitter();
+
+  ngOnInit(): void {
+
+  }
+
+  onAddToCart(): void {
+    this.addToCart.emit(this.product);
+  }
 
 }
