e4c51fda75e7b2d2fa93a33a0b4570948034de2d
LudovicDupre <dupreludovic1@gmail.com>
2023-04-24 20:49:16 +0200

    regression on logic commit lol

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Cart, CartItem } from 'src/app/models/cart.model';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -15,7 +16,7 @@ export class CartComponent implements OnInit {
       price: 150,
       quantity: 1,
       id: 1,
-    },{
+    }, {
       product: 'https://via.placeholder.com/150',
       name: 'sneakers',
       price: 150,
@@ -33,7 +34,7 @@ export class CartComponent implements OnInit {
     'total',
     'action'
   ]
-  constructor() { }
+  constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
     this.dataSource = this.cart.items;
@@ -41,8 +42,6 @@ export class CartComponent implements OnInit {
   }
 
   getTotal(items: Array<CartItem>): number {
-    return items.
-      map((item) => item.price * item.quantity)
-      .reduce((prev, current) => prev + current, 0);
+    return this.cartService.getTotal(items);
   }
 }
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -15,7 +15,7 @@ export class CartService {
   addToCart(item: CartItem): void {
     const items = [...this.cart.value.items];
 
-    const itemInCart = items.find((_item)=> _item.id === item.id);
+    const itemInCart = items.find((_item) => _item.id === item.id);
 
     if (itemInCart) {
       itemInCart.quantity += 1;
@@ -23,8 +23,20 @@ export class CartService {
       items.push(item);
     }
 
-    this.cart.next({items});
-    this._snackBar.open('1 item added to cart.','OK',{ duration: 3000 });
+    this.cart.next({ items });
+    this._snackBar.open('1 item added to cart.', 'OK', { duration: 3000 });
     console.log(this.cart.value);
+  }
+
+  getTotal(items: Array<CartItem>): number {
+    return items.
+      map((item) => item.price * item.quantity)
+      .reduce((prev, current) => prev + current, 0);
+  }
+  clearCart(): void {
+    this.cart.next({ items: [] });
+    this._snackBar.open('Cart is cleared.', 'Ok', {
+      duration: 3000
+    })
   }
 }
